b415529f4848e74c2ebb344943cd2f318e671ed3
Ye Yu <nearjilt@gmail.com>
2024-10-17 19:06:22 +0800

    day 1, only-one-selected

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FuckCSS from './components/day1-only-one/FuckCSS'
+
+function App() {
+  return (
+    <div className="bg-blue-100 h-screen w-screen flex">
+      <FuckCSS></FuckCSS>
+    </div>
+  )
+}
+
+export default App
